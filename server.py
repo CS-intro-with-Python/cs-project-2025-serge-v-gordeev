9226c16d5815bbf29b785235616abd292875577b
serge <serge.v.gordeev@gmail.com>
2026-01-02 09:12:43 +0200

    Will the action work this time or not?

--- server.py
+++ server.py
@@ -8,5 +8,4 @@ def temp():
     return ('This is a test message, there is nothing here yet')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
-
+    app.run(host="0.0.0.0", port=28931)
