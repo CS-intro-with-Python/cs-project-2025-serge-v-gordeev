2a67eeac13dc603c85175f259ca8dd31acca5189
serge <serge.v.gordeev@gmail.com>
2026-01-15 20:32:53 +0200

    Requirements 4

--- test1.py
+++ test1.py
@@ -1,46 +1,43 @@
 from flask import Flask, jsonify, request, render_template
-#from dv_calculator import get_response, hardcode_planets
-#from database import db
+from dv_calculator import get_response, hardcode_planets
+from database import db
 import logging
 
-#logging.basicConfig(
-#    level=logging.INFO,
-#    format="%(asctime)s %(levelname)s %(name)s: %(message)s"
-#)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s"
+)
 
 app = Flask(__name__)
-#app.name = "Semester 1 project"
+app.name = "Semester 1 project"
 
-#werkzeug_logger = logging.getLogger("werkzeug")
-#werkzeug_logger.setLevel(logging.WARNING)
-#handler = logging.StreamHandler()
-#formatter = logging.Formatter("%(asctime)s%(levelname)s %(name)s: %(message)s")
-#werkzeug_logger.addHandler(handler)
-#handler.setFormatter(formatter)
+werkzeug_logger = logging.getLogger("werkzeug")
+werkzeug_logger.setLevel(logging.WARNING)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s%(levelname)s %(name)s: %(message)s")
+werkzeug_logger.addHandler(handler)
+handler.setFormatter(formatter)
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ksp_celestial_body_data" # "postgresql://postgres:your_password@localhost:5432/ksp_celestial_body_data"
-#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#db.init_app(app)
-#with app.app_context():
-#    db.create_all()
-#    hardcode_planets()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ksp_celestial_body_data" # "postgresql://postgres:your_password@localhost:5432/ksp_celestial_body_data"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+    hardcode_planets()
 
-#@app.before_request
-#def log():
-#    app.logger.info("Incoming %s request from %s on path %s", request.method, request.remote_addr, request.path)
+@app.before_request
+def log():
+    app.logger.info("Incoming %s request from %s on path %s", request.method, request.remote_addr, request.path)
 
 @app.route('/')
 def temp():
-    return "abcd"
-    #return render_template("form.html")
+    return render_template("form.html")
 
-#@app.route('/test', methods=["POST"])
-#def test():
-#    received = request.json
-#    print(received)
-#    to_return = "abcd"
-#    #to_return = jsonify(get_response(received))
-#    return to_return
+@app.route('/test', methods=["POST"])
+def test():
+    received = request.json
+    to_return = jsonify(get_response(received))
+    return to_return
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=31764)
