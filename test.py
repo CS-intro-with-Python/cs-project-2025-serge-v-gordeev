1fa2ba6311cc0402217de543d4e94dcc0d1fbc25
serge <serge.v.gordeev@gmail.com>
2026-01-15 18:40:11 +0200

    Empty test 2

--- test.py
+++ test.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 app.name = "Semester 1 project bare test"
 
+@app.route("/")
+def index():
+    return "OK", 200
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=31764)
